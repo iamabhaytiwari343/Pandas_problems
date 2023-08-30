8b5bdf35bc71c1e168512edf262500b226499918
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-08-30 07:31:50 +0530

    the number of rich customers

---
+++ P_2082.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def count_rich_customers(store: pd.DataFrame) -> pd.DataFrame:
+    rich_count = store.loc[store['amount'] > 500, 'customer_id'].nunique()
+    return pd.DataFrame({'rich_count': [rich_count]})
