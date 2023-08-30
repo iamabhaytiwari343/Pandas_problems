d4ebfcebcd2ddb93e9f2f79de979b8fd30a2a7de
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-08-30 07:49:48 +0530

    game play analysis

--- P_511.py
+++ P_511.py
@@ -5,3 +5,4 @@ def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
     first_login = activity.groupby(
         'player_id')['event_date'].min().reset_index(name='first_login')
     return first_login
+
