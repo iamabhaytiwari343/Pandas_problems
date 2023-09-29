c2a26321f6becbcdb18ecff32a3de9708848adef
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-09-30 00:22:30 +0530

    find customers who never order

--- P_183.py
+++ P_183.py
@@ -2,9 +2,13 @@ import pandas as pd
 
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    c = customers.merge(orders, left_on='id',
-                        right_on='customerId', how='left')
-    c = c[c['id_y'].isnull()]
-    c = pd.DataFrame(c['name'])
-    c = c.rename(columns={'name': 'customers'})
+    # c = customers.merge(orders, left_on='id',
+    #                     right_on='customerId', how='left')
+    # c = c[c['id_y'].isnull()]
+    # c = pd.DataFrame(c['name'])
+    # c = c.rename(columns={'name': 'customers'})
+    # return c
+
+    c=customers.merge(orders, left_on='id', right_on='customerId', how='left')
+    c = pd.DataFrame(c[c['id_y'].isnull()]['name']).rename(columns={'name': 'customers'})
     return c
