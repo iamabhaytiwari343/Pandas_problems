19bf8b6f19279af6281a0dd57aa3645ef67e2657
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-09-30 00:19:34 +0530

    customer who never order

--- P_183.py
+++ P_183.py
@@ -2,11 +2,9 @@ import pandas as pd
 
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    never_ordered_customers = customers.merge(
-        orders, left_on='id', right_on='customerId', how='left')
-    never_ordered_customers = never_ordered_customers[never_ordered_customers['customerId'].isnull(
-    )]
-    result = never_ordered_customers[['name']].rename(
-        columns={'name': 'Customers'})
-    d = pd.DataFrame(result)
-    return d
+    c = customers.merge(orders, left_on='id',
+                        right_on='customerId', how='left')
+    c = c[c['id_y'].isnull()]
+    c = pd.DataFrame(c['name'])
+    c = c.rename(columns={'name': 'customers'})
+    return c
