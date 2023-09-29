07f6306a64b0607a3c49e0fc8de9b6c6ce2fd1cf
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-09-30 00:17:31 +0530

    big countries

--- P_595.py
+++ P_595.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
 
-def big_countries(data: pd.DataFrame) -> pd.DataFrame:
-    big_countries = data[(data['area'] >= 3000000) |
-                         (data['population'] >= 25000000)]
-    result = big_countries[['name', 'population', 'area']]
-    d = pd.DataFrame(result)
-    return d
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    world = world[(world['area'] >= 3000000) |
+                  (world['population'] >= 25000000)]
+    world = world[['name', 'population', 'area']]
+    return world
